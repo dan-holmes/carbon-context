ac92edcf4f1b999e4d8734c7ea5fb662fb1ebf96
dan-holmes <dh@danielholmes.com>
2021-02-24 16:31:23 +0000

    Create circle sizes based on values

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {Circle} from 'react-shapes';
+
+let data = {
+  "Lightbulb for one hour": 2,
+  "Fly one mile": 100,
+  "Eat 1kg of steak": 14
+};
+
+function createCircles(data) {
+  let circles = []
+  for (const [key, value] of Object.entries(data)) {
+    circles.push(<Circle r={value} fill={{color:'#2409ba'}} stroke={{color:'#E65243'}} strokeWidth={3} />);
+  }
+  return circles;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {createCircles(data)}
     </div>
   );
 }
